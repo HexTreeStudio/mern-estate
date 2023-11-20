4cb62ca9eedfdfd892add3b6c94cfe16914e3eb7
HexTreeStudio <lukasplesnik@gmail.com>
2023-11-20 17:01:56 +0100

    create signup route

---
+++ api/routes/user.route.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { test } from "../controlers/user.controller.js";
+
+
+
+const router = express.Router();
+
+router.get("/test", test)
+
+export default router;
